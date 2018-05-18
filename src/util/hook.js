70ebce21db4d2f54c0f9b3fcf71e5941368cb343
erha19 <faterrole@gmail.com>
2018-05-18 17:55:09 +0800

    Release v1.0.7

--- src/util/hook.js
+++ src/util/hook.js
@@ -13,63 +13,59 @@ exports.record = function (logkey, goldkey) {
     node: process.version
   };
   goldkey = Object.assign(defaultOptions, goldkey);
-  dns.resolve('http://gm.mmstat.com', function (err) {
+  dns.resolve('gm.mmstat.com', function (err) {
     if (!err) {
       request({
-        method: 'GET',
-        uri: 'http://httpbin.org/ip'
+        method: 'POST',
+        uri: 'http://ip.taobao.com/service/getIpInfo2.php',
+        headers: {
+          'content-type': 'application/x-www-form-urlencoded'
+        },
+        form: {
+          ip: 'myip'
+        },
+        json: true
       })
-        .then((remote) => {
-          remote = JSON.parse(remote);
+        .then((body) => {
+          console.log(body);
+          // Request succeeded but might as well be a 404
+          // Usually combined with resolveWithFullResponse = true to check response.statusCode
+          goldkey['ip'] = body.data.ip;
+          goldkey['country'] = utf8.encode(body.data.country);
+          goldkey['region'] = utf8.encode(body.data.region);
+          goldkey['city'] = utf8.encode(body.data.city);
+          goldkey['county'] = utf8.encode(body.data.county);
+          goldkey['isp'] = utf8.encode(body.data.isp);
+          goldkey['_g_encode'] = 'utf-8';
+          let url = `http://gm.mmstat.com${logkey}?`;
+          for (const i in goldkey) {
+            if (goldkey.hasOwnProperty(i)) {
+              url += `${i}=${goldkey[i]}&`;
+            }
+          }
+          url += `t=${(new Date()).getTime()}`;
+          console.log(url);
           request({
             method: 'GET',
-            uri: `http://ip.taobao.com/service/getIpInfo2.php?ip=${remote.origin}`
-          }).then((body) => {
-            // Request succeeded but might as well be a 404
-            // Usually combined with resolveWithFullResponse = true to check response.statusCode
-            body = JSON.parse(body);
-            goldkey['ip'] = remote.origin;
-            goldkey['country'] = utf8.encode(body.data.country);
-            goldkey['region'] = utf8.encode(body.data.region);
-            goldkey['city'] = utf8.encode(body.data.city);
-            goldkey['county'] = utf8.encode(body.data.county);
-            goldkey['isp'] = utf8.encode(body.data.isp);
-            goldkey['_g_encode'] = 'utf-8';
-            let url = `http://gm.mmstat.com${logkey}?`;
-            for (const i in goldkey) {
-              if (goldkey.hasOwnProperty(i)) {
-                url += `${i}=${goldkey[i]}&`;
-              }
-            }
-            url += `t=${(new Date()).getTime()}`;
-            request({
-              method: 'GET',
-              uri: url
-            })
-              .then(() => {})
-              .catch(function (err) {
-                // Request failed due to technical reasons...
-                request({
-                  method: 'GET',
-                  uri: `http://gm.mmstat.com/weex_tool.weex-toolkit.error_track?error=${err}&stack=${err && err.stack}`
-                }).then(() => {});
-              });
+            uri: url
           })
+            .then(() => {})
             .catch(function (err) {
               // Request failed due to technical reasons...
               request({
                 method: 'GET',
                 uri: `http://gm.mmstat.com/weex_tool.weex-toolkit.error_track?error=${err}&stack=${err && err.stack}`
-              })
-                .then(() => {});
+              }).then(() => {});
             });
         })
         .catch(function (err) {
+          console.log(err);
           // Request failed due to technical reasons...
           request({
             method: 'GET',
             uri: `http://gm.mmstat.com/weex_tool.weex-toolkit.error_track?error=${err}&stack=${err && err.stack}`
-          }).then(() => {});
+          })
+            .then(() => {});
         });
     }
   });
